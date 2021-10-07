b89edb9107fd6f9e4b8517a2533b040ad7e5da4a
vkavirathna <vimaya.18@business.mrt.ac.lk>
2021-10-07 22:12:03 +0530

    Calculator class created

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+class Calculator:
+    def add(self, x, y):
+        return x + y
+
+    def factorial(self, n):
+        if (n == 0):
+            return 1
+        else:
+            return self.factorial(n - 1) * n
+
