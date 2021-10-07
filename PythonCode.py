cc767fceb3cb2cd8fef834c6b09e445c21e38b1b
vkavirathna <vimaya.18@business.mrt.ac.lk>
2021-10-07 22:19:05 +0530

    Commiting missing changes

--- PythonCode.py
+++ PythonCode.py
@@ -1,3 +1,11 @@
-value = 10
-print('Hello')
-print(value)
+from Calculator import Calculator
+
+
+def main():
+    Cal = Calculator()
+    print(Cal.add(2, 3))
+    print(Cal.add(7, 3))
+    print(Cal.multiply(5, 3))
+
+if __name__ == "__main__":
+    main()
