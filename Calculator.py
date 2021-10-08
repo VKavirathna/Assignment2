2cd455819ba6fd762d07a0cb9b151c3740431e15
BuddhiChathurika <92119094+BuddhiChathurika@users.noreply.github.com>
2021-10-08 12:24:18 +0530

    Update Calculator.py
    Including the divide function

--- Calculator.py
+++ Calculator.py
@@ -5,11 +5,17 @@ class Calculator:
     def substract(self, x, y):
         return x - y
     
+    def multiply(self, x, y):
+        return x * y
+    
+    def Divide(self, x, y):
+        return x / y
+    
     def factorial(self, n):
         if (n == 0):
             return 1
         else:
             return self.factorial(n - 1) * n
+        
 
-    def multiply(self, x, y):
-        return x * y
+    
